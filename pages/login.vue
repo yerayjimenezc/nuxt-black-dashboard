92d956c76e80486682b511927f4409fab58f599d
yerayjimenezc <yerayjimenezcarmona@gmail.com>
2023-02-12 19:20:46 +0100

    add input register users

--- pages/login.vue
+++ pages/login.vue
@@ -1,30 +1,61 @@
 <template>
-    <v-card>
+    <card>
         <h1>Soy un login</h1>
-        <v-btn @click="login">LogIn</v-btn>
-        <v-btn @click="logout">LogOut</v-btn>
-    </v-card>
+        <button class="btn btn-secundary" @click="login">LogIn</button>
+        <button class="btn btn-secundary" @click="logout">LogOut</button>
+        <card>
+            <h1 class="card-title">Registro de usuarios</h1>
+            <form>
+                <label>Usuario:</label>
+                <input type="text" placeholder="nombre de usuario" v-model="user">
+                <label>password:</label>
+                <input type="text" placeholder="****************" v-model="password">
+                <label>Role:</label>
+                <select v-model="role" class="form-select">
+                    <option value="user">user</option>
+                    <option value="admin">admin</option>
+                </select>
+                <button type="button" @click="register">Registrar</button>
+            </form>
+        </card>
+    </card>
 </template>
 
 <script>
+import axios from "axios"
+const localAPI = axios.create({
+    baseURL: "http://localhost:8000",
+})
 export default {
     components: {
 
     },
     data () {
         return {
-            isLoggedIn: false
+            isLoggedIn: false,
+            user: '',
+            password: '',
+            role: ''
         }
     },
     methods: {
         login () {
-            this.isLoggedIn = true
             localStorage.setItem('isLoggedIn', true)
             this.$router.push('/')
         },
         logout () {
-            this.isLoggedIn = false
             localStorage.removeItem('isLoggedIn')
+        },
+        async register () {
+            try {
+                const sendData = { name: this.user, password: this.password, role: this.role }
+                const { data } = await localAPI.post("/register", sendData);
+                localStorage.setItem(data)
+                console.log(data)
+            } catch (error) {
+                console.error(error)
+                return false
+            }
         }
     }
 }
